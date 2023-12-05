06ad9e44410c598b5ce2129b2ee65724ee7bb054
Juan Pablo Chavez Arteaga <juanpablo.chavez@training.assuresoft.com>
2023-12-05 09:45:46 -0400

    Eslint added to the project

--- src/routes/client.route.ts
+++ src/routes/client.route.ts
@@ -1,27 +1,27 @@
-import { Router } from "express";
-import ClientController from "../controllers/client.controller";
-import { AuthMiddleware, ModelRouter } from "../types/types";
-import JWTMiddleware from "../middleware/jwt.auth.middleware";
+import { Router } from 'express';
+import ClientController from '../controllers/client.controller';
+import { AuthMiddleware, ModelRouter } from '../types/types';
+import JWTMiddleware from '../middleware/jwt.auth.middleware';
 
-export default class ClientRouter implements ModelRouter{
-  private router: Router = Router()
-  private controller: ClientController
-  private authMiddleware: AuthMiddleware
+export default class ClientRouter implements ModelRouter {
+  private router: Router = Router();
+  private controller: ClientController;
+  private authMiddleware: AuthMiddleware;
 
-  public readonly PATH = '/clients'
+  public readonly PATH = '/clients';
 
   public constructor(controller: ClientController) {
-    this.authMiddleware = new JWTMiddleware()
+    this.authMiddleware = new JWTMiddleware();
 
-    this.controller = controller
-    this.router.get('/', this.authMiddleware.validateToken, this.controller.getInfoClient)
-    this.router.post('/', this.controller.createClient)
-    this.router.post('/auth', this.controller.loginClient)
+    this.controller = controller;
+    this.router.get('/', this.authMiddleware.validateToken, this.controller.getInfoClient);
+    this.router.post('/', this.controller.createClient);
+    this.router.post('/auth', this.controller.loginClient);
     // this.router.delete('/:id', this.controller.deleteClient)
   }
 
   public getRouterPath() {
-    return { router: this.router, path: this.PATH }
+    return { router: this.router, path: this.PATH };
   }
 
 }
